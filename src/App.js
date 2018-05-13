e4edd863e9987b74b314b4a60496b54ec293ff93
aekkawan <5730213083@email.psu.ac.th>
2018-05-14 02:00:29 +0700

    rename component folder

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import WeekTable from './Component/WeekTable'
+import WeekTable from './components/WeekTable'
 
 import { connect } from "react-redux";
 
