e5e91a33e798bd1ada1a95bc85fedc5190509e68
aekkawan <5730213083@email.psu.ac.th>
2018-05-14 05:15:20 +0700

    Update Main App and  WeekTable

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import WeekTable from './components/WeekTable'
-
 import { connect } from "react-redux";
 
 class App extends Component {
@@ -10,21 +9,46 @@ class App extends Component {
     this.props.onRequestUsers()
   }
 
+  parseDay = (timestamp) => {
+    return new Date(timestamp * 1000).getDay()
+  }
+
+  findDay = (users = []) => {
+    const sunData = [], monData = [], tueData = [], wedData = [], thuData = [], friData = [], satData = []
+    for (let user of users) {
+      let rawDate = user.birthday.raw
+      if (this.parseDay(rawDate) === 0) sunData.push(user)
+      if (this.parseDay(rawDate) === 1) monData.push(user)
+      if (this.parseDay(rawDate) === 2) tueData.push(user)
+      if (this.parseDay(rawDate) === 3) wedData.push(user)
+      if (this.parseDay(rawDate) === 4) thuData.push(user)
+      if (this.parseDay(rawDate) === 5) friData.push(user)
+      if (this.parseDay(rawDate) === 6) satData.push(user)
+    }
+    const sun = Object.assign({}, { group_name: "sunday", data: sunData }),
+      mon = Object.assign({}, { group_name: "monday", data: monData }),
+      tue = Object.assign({}, { group_name: "tuesday", data: tueData }),
+      wed = Object.assign({}, { group_name: "wednesday", data: wedData }),
+      thu = Object.assign({}, { group_name: "thursday", data: thuData }),
+      fri = Object.assign({}, { group_name: "friday", data: friData }),
+      sat = Object.assign({}, { group_name: "saturday", data: satData }),
+      objUsers = [{ ...sun }, { ...mon }, { ...tue }, { ...wed }, { ...thu }, { ...fri }, { ...sat }]
+    return objUsers
+  }
+
   render() {
     const { fetching, users, error } = this.props;
-    users ? console.log(users) : console.log(users)
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">(React) Manipulate Data Demo by Aekkawan</h1>
         </header>
-        <p className="App-intro"> </p>
         {
-          fetching === true ? <div>loading.....</div> : null
+          fetching === true ? <p className="App-intro">loading.....</p> : null
         }
         {
-          users ? <WeekTable data={this.props.users} /> : null
+          users ? <WeekTable data={this.findDay(this.props.users)} /> : null
         }
         {
           error ? <div>error.....</div> : null
--- src/components/WeekTable.js
+++ src/components/WeekTable.js
@@ -1,56 +1,19 @@
-import React, { Component } from 'react';
+import React from 'react';
 import WeekColumn from './WeekColumn'
 
-export default class WeekTable extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            monData: [],
-            tueData: [],
-            wedData: [],
-            thuData: [],
-            friData: [],
-            satData: [],
-            sunData: [],
-        }
+export default function WeekTable({ data }) {
+    
+    const itemList = data.map((user, i) => <WeekColumn key={i} title={createTitle(user.group_name)} data={user.data} />)
+
+    function createTitle(name) {
+        return name.substr(0, 3).toUpperCase()
     }
 
-    componentDidMount() {
-        this.findDay(this.props.data)
-    }
-
-    parseDay = (timestamp) => {
-        const day = new Date(timestamp * 1000).getDay()
-        return day
-    }
-
-    findDay = (users = []) => {
-        let sunData = [], monData = [], tueData = [], wedData = [], thuData = [], friData = [], satData = []
-        for (let user of users) {
-            let rawDate = user.birthday.raw
-            if (this.parseDay(rawDate) === 0) sunData.push(user)
-            if (this.parseDay(rawDate) === 1) monData.push(user)
-            if (this.parseDay(rawDate) === 2) tueData.push(user)
-            if (this.parseDay(rawDate) === 3) wedData.push(user)
-            if (this.parseDay(rawDate) === 4) thuData.push(user)
-            if (this.parseDay(rawDate) === 5) friData.push(user)
-            if (this.parseDay(rawDate) === 6) satData.push(user)
-        }
-        this.setState({ sunData, monData, tueData, wedData, thuData, friData, satData })
-    }
-
-    render() {
-        let { sunData, monData, tueData, wedData, thuData, friData, satData } = this.state
-        return (
-            <div className="container">
-                <WeekColumn title={"SUN"} data={sunData} />
-                <WeekColumn title={"MON"} data={monData} />
-                <WeekColumn title={"TUE"} data={tueData} />
-                <WeekColumn title={"WED"} data={wedData} />
-                <WeekColumn title={"THU"} data={thuData} />
-                <WeekColumn title={"FRI"} data={friData} />
-                <WeekColumn title={"SAT"} data={satData} />
-            </div>
-        )
-    }
+    return (
+        <div className="container">
+            {
+                itemList
+            }
+        </div>
+    )
 }
