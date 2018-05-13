d15463233dfb574994f0357e4d2c24f62aee5431
aekkawan <5730213083@email.psu.ac.th>
2018-05-14 01:18:14 +0700

    update index

--- src/index.js
+++ src/index.js
@@ -3,11 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-
 import { createStore, applyMiddleware, compose } from "redux";
 import createSagaMiddleware from "redux-saga";
 import { Provider } from "react-redux";
-
 import { reducer } from "./redux";
 import { watcherSaga } from "./sagas";
 
